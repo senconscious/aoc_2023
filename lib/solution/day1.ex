a05b876964e4fabcd56c99dff424a727d77b2336
Artem Baikov <ivankolga25@gmail.com>
2023-12-10 11:12:50 +0300

    add day1 part 2 prototype

--- lib/solution/day1.ex
+++ lib/solution/day1.ex
@@ -5,6 +5,14 @@ defmodule Solution.Day1 do
 
   @path "priv/inputs/day1"
 
+  @string_digits for integer <- 1..9, do: Integer.to_string(integer)
+
+  @word_digits ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+  @word_digits_map @word_digits |> Enum.with_index(1) |> Map.new()
+
+  @pattern ~r/\d|one|two|three|four|five|six|seven|eight|nine/
+
   def calculate_value(path \\ @path) do
     path
     |> File.stream!()
@@ -13,9 +21,11 @@ defmodule Solution.Day1 do
   end
 
   defp first_last_digits_tuple(string) when is_binary(string) do
-    occurences = Regex.scan(~r/\d/, string)
+    IO.inspect(string, label: "Input string")
+    occurences = Regex.scan(@pattern, string)
 
     {first(occurences), last(occurences)}
+    |> IO.inspect(label: "Output string")
   end
 
   defp first(occurences) do
@@ -30,8 +40,12 @@ defmodule Solution.Day1 do
     |> to_integer()
   end
 
-  defp to_integer([digit]) when is_binary(digit) do
+  defp to_integer([digit]) when digit in @string_digits do
     String.to_integer(digit)
+  end
+
+  defp to_integer([digit]) when digit in @word_digits do
+    Map.fetch!(@word_digits_map, digit)
   end
 
   defp to_number({first, last}, acc) when is_integer(first) and is_integer(last) do
--- test/solution/day1_test.exs
+++ test/solution/day1_test.exs
@@ -3,7 +3,11 @@ defmodule Solution.Day1Test do
 
   alias Solution.Day1
 
-  test "calculates calibrator" do
-    assert 142 = Day1.calculate_value("test/support/inputs/day1")
+  test "calculates calibrator only digits" do
+    assert 142 = Day1.calculate_value("test/support/inputs/day1_half1")
+  end
+
+  test "calculates calibrator not only digits" do
+    assert 281 = Day1.calculate_value("test/support/inputs/day1_half2")
   end
 end
