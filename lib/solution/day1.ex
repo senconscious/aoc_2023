ae1989ddb53445347bffaaa2cdfe86b095c74fdf
Artem Baikov <ivankolga25@gmail.com>
2023-12-10 11:50:37 +0300

    fix day1 overlap letter digit issue

--- lib/solution/day1.ex
+++ lib/solution/day1.ex
@@ -5,13 +5,7 @@ defmodule Solution.Day1 do
 
   @path "priv/inputs/day1"
 
-  @string_digits for integer <- 1..9, do: Integer.to_string(integer)
-
-  @word_digits ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-
-  @word_digits_map @word_digits |> Enum.with_index(1) |> Map.new()
-
-  @pattern ~r/\d|one|two|three|four|five|six|seven|eight|nine/
+  @pattern ~r/\d/
 
   def calculate_value(path \\ @path) do
     path
@@ -21,12 +15,25 @@ defmodule Solution.Day1 do
   end
 
   defp first_last_digits_tuple(string) when is_binary(string) do
-    IO.inspect(string, label: "Input string")
-    occurences = Regex.scan(@pattern, string)
+    occurences =
+      string
+      |> replace_letter_digits()
+      |> then(&Regex.scan(@pattern, &1))
 
     {first(occurences), last(occurences)}
-    |> IO.inspect(label: "Output string")
   end
+
+  defp replace_letter_digits("one" <> rest), do: "1" <> replace_letter_digits("e" <> rest)
+  defp replace_letter_digits("two" <> rest), do: "2" <> replace_letter_digits("o" <> rest)
+  defp replace_letter_digits("three" <> rest), do: "3" <> replace_letter_digits("e" <> rest)
+  defp replace_letter_digits("four" <> rest), do: "4" <> replace_letter_digits("r" <> rest)
+  defp replace_letter_digits("five" <> rest), do: "5" <> replace_letter_digits("e" <> rest)
+  defp replace_letter_digits("six" <> rest), do: "6" <> replace_letter_digits("x" <> rest)
+  defp replace_letter_digits("seven" <> rest), do: "7" <> replace_letter_digits("n" <> rest)
+  defp replace_letter_digits("eight" <> rest), do: "8" <> replace_letter_digits("t" <> rest)
+  defp replace_letter_digits("nine" <> rest), do: "9" <> replace_letter_digits("e" <> rest)
+  defp replace_letter_digits(<<char, rest::binary>>), do: <<char>> <> replace_letter_digits(rest)
+  defp replace_letter_digits(""), do: ""
 
   defp first(occurences) do
     occurences
@@ -40,12 +47,8 @@ defmodule Solution.Day1 do
     |> to_integer()
   end
 
-  defp to_integer([digit]) when digit in @string_digits do
+  defp to_integer([digit]) do
     String.to_integer(digit)
-  end
-
-  defp to_integer([digit]) when digit in @word_digits do
-    Map.fetch!(@word_digits_map, digit)
   end
 
   defp to_number({first, last}, acc) when is_integer(first) and is_integer(last) do
